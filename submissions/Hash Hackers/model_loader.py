9f3dc31b7ab6a3b31af8df6c8054d73a709b2c0e
Ashu <97926707+Ashu-Viron@users.noreply.github.com>
2025-04-17 18:57:37 +0530

    Update model_loader.py

--- submissions/Hash Hackers/model_loader.py
+++ submissions/Hash Hackers/model_loader.py
@@ -1,8 +1,8 @@
 import torch
 from model import TinyResNet 
 
-def load_model():
+def load_model(model_path):
     model = TinyResNet(num_classes=100)
-    model.load_state_dict(torch.load("model_final.pth", map_location="cpu"))
+    model.load_state_dict(torch.load("model_path", map_location="cpu"))
     model.eval()
     return model
