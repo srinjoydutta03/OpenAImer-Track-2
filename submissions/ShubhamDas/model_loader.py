1689bcc6b703fd2849424eada35d279068a5bf2b
sayanta-nandi <sayantanandi123@gmail.com>
2025-04-17 21:38:23 +0530

    1st model submission of Trinity Thinkers changes made as suggested 3

--- submissions/ShubhamDas/model_loader.py
+++ submissions/ShubhamDas/model_loader.py
@@ -1,24 +1,28 @@
-import os
+# import os
 import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-import time
+# import torchvision
+# import torchvision.transforms as transforms
+# from torch.utils.data import DataLoader
+# import time
 
 # Auto device selection
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # print(f"🖥️ Using device: {device}")
 
 # Load compressed half-precision model
-model = torch.jit.load('/content/model.pt', map_location=device)
-model.eval()
+def load_model(model_path="model.pt",device=None):
+    if device==None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = torch.jit.load(model_path, map_location=device)
+    model.eval()
+    return model
 
 # Data transforms
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
+# transform = transforms.Compose([
+#     transforms.Resize((224, 224)),
+#     transforms.ToTensor(),
+# ])
 
 # Dataset loader
-dataset = torchvision.datasets.ImageFolder(root='/content/archive/', transform=transform)
-loader = DataLoader(dataset, batch_size=32, shuffle=False)
+# dataset = torchvision.datasets.ImageFolder(root='/content/archive/', transform=transform)
+# loader = DataLoader(dataset, batch_size=32, shuffle=False)
