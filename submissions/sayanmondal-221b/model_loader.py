8825de21c04787ad4928aab14ef89c2b2f42f5b7
sayanmondal-221b <sayanmondal221b@gmail.com>
2025-04-17 21:04:19 +0530

    Update model_loader.py

--- submissions/sayanmondal-221b/model_loader.py
+++ submissions/sayanmondal-221b/model_loader.py
@@ -1,4 +1,3 @@
-# model_loader.py
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18
@@ -6,7 +5,12 @@ from torch.quantization import quantize_dynamic
 
 def load_model(model_path):
     model = resnet18()
-    model = quantize_dynamic(model, {nn.Linear}, dtype=torch.qint8)
     model.load_state_dict(torch.load(model_path, map_location='cpu'))
     model.eval()
-    return model
+    model_quantized = quantize_dynamic(
+        model,  
+        {nn.Linear},  
+        dtype=torch.qint8 
+    )
+
+    return model_quantized
